72f846201693c8839a51dd0c13f5b322f4c2e581
Blas <slab10000@gmail.com>
2024-05-31 01:05:46 +0200

    First take on the counter

--- app/src/main/java/com/blas/romanempirecounter/MainActivity.kt
+++ app/src/main/java/com/blas/romanempirecounter/MainActivity.kt
@@ -3,11 +3,12 @@ package com.blas.romanempirecounter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.blas.romanempirecounter.ui.theme.RomanEmpireCounterTheme
@@ -17,13 +18,17 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             RomanEmpireCounterTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                val counter = remember { mutableIntStateOf(0) }
+                Column {
+                    Text(text = "Hello " + intToRoman(counter.intValue))
+                    println(counter.intValue)
+                    Button(
+                        onClick = {counter.intValue++},
+                    ){
+                        Text(text = "Hello")
+                    }
                 }
+
             }
         }
     }
@@ -43,4 +48,24 @@ fun GreetingPreview() {
     RomanEmpireCounterTheme {
         Greeting("Android")
     }
+}
+
+fun intToRoman(num: Int): String {
+    val romanNumerals = listOf(
+        Pair(1000, "M"), Pair(900, "CM"), Pair(500, "D"), Pair(400, "CD"),
+        Pair(100, "C"), Pair(90, "XC"), Pair(50, "L"), Pair(40, "XL"),
+        Pair(10, "X"), Pair(9, "IX"), Pair(5, "V"), Pair(4, "IV"), Pair(1, "I")
+    )
+
+    var number = num
+    val stringBuilder = StringBuilder()
+
+    for ((value, symbol) in romanNumerals) {
+        while (number >= value) {
+            stringBuilder.append(symbol)
+            number -= value
+        }
+    }
+
+    return stringBuilder.toString()
 }
