6106a554c460c5ff6627df73acc36e9a5826464e
Blas <slab10000@gmail.com>
2024-09-29 00:30:17 +0200

    Cambiar el color de la statusbar

--- app/src/main/java/com/blas/romanempirecounter/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/blas/romanempirecounter/presentation/ui/theme/Theme.kt
@@ -5,6 +5,7 @@ import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
@@ -42,24 +43,25 @@ private val LightColorScheme = lightColorScheme(
 fun RomanEmpireCounterTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
-            /*if (darkTheme) dynamicDarkColorScheme(context) else*/ dynamicLightColorScheme(context)
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
-
-        //darkTheme -> DarkColorScheme
+        darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = LightColorScheme.tertiary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            window.statusBarColor = LightColorScheme.tertiary.toArgb() // Cambiar color de la StatusBar
+
+            // Configurar el color del texto en la StatusBar (claro u oscuro)
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = false
         }
     }
 
