f80e36a2565e247fc1242fd830c1e5182daf2500
Blas <slab10000@gmail.com>
2024-09-29 00:31:22 +0200

    Change version to 1.4 to create a new Release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.blas.romanempirecounter"
         minSdk = 26
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.3"
+        versionCode = 4
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
