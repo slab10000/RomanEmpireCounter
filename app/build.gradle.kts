1e812259f14d5596be25e98b90f4042f592d1344
Blas <slab10000@gmail.com>
2024-11-19 21:47:58 +0100

    Release version 1.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.blas.romanempirecounter"
         minSdk = 26
         targetSdk = 34
-        versionCode = 6
-        versionName = "1.6"
+        versionCode = 7
+        versionName = "1.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
